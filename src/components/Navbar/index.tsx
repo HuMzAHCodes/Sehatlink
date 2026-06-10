3cf3859ca6f625314f073c170ed87d2b21d5bd5e
HAmzaaider <kohat3066@gmail.com>
2026-06-10 23:33:27 +0500

    Adding Aniamtion to NAVABAR

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useRef } from "react";
 import NavLogo from "./NavLogo";
 import NavLinks from "./NavLinks";
 import NavActions from "./NavActions";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const navRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (navRef.current) {
+        navRef.current.classList.toggle(styles.scrolled, window.scrollY > 20);
+      }
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className={styles.navbar}>
+    <header ref={navRef} className={styles.navbar}>
       <NavLogo />
       <NavLinks />
       <NavActions />
